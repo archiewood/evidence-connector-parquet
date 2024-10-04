e479a59101cd203b9d6876b1f0f3e438459874a8
Archie Wood <58074498+archiewood@users.noreply.github.com>
2024-10-04 11:45:08 +0200

    working csv connector, ESM

--- src/index.js
+++ src/index.js
@@ -1,115 +1,56 @@
-/**
- * This type describes the options that your connector expects to recieve
- * This could include username + password, host + port, etc
- * @typedef {Object} ConnectorOptions
- * @property {string} SomeOption
- */
-
-import { EvidenceType } from "@evidence-dev/db-commons";
+import { exhaustStream } from '@evidence-dev/db-commons';
+import runQuery from '@evidence-dev/duckdb';
 
 /**
- * @see https://docs.evidence.dev/plugins/create-source-plugin/#options-specification
- * @see https://github.com/evidence-dev/evidence/blob/main/packages/postgres/index.cjs#L316
+ * @typedef {Object} DuckDBOptions
+ * @property {string} options
  */
-export const options = {
-  SomeOption: {
-    title: "Some Option",
-    description:
-      "This object defines how SomeOption should be displayed and configured in the Settings UI",
-    type: "string", // options: 'string' | 'number' | 'boolean' | 'select' | 'file'
-  },
+
+/** @type {import("@evidence-dev/db-commons").RunQuery<DuckDBOptions>} */
+export default async (queryString, _, batchSize = 100000) => {
+	return runQuery(queryString, { filename: ':memory:' }, batchSize);
 };
 
-/**
- * Implementing this function creates a "file-based" connector
- *
- * Each file in the source directory will be passed to this function, and it will return
- * either an array, or an async generator {@see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function*}
- * that contains the query results
- *
- * @see https://docs.evidence.dev/plugins/create-source-plugin/
- * @type {import("@evidence-dev/db-commons").GetRunner<ConnectorOptions>}
- */
-export const getRunner = (options) => {
-  console.debug(`SomeOption = ${options.SomeOption}`);
+/** @type {import("@evidence-dev/db-commons").GetRunner<DuckDBOptions>} */
+export const getRunner = ({ options }) => {
+	return async (queryContent, queryPath, batchSize) => {
+		// Filter out non-csv files
+		if (!queryPath.endsWith('.csv')) return null;
+		// Use DuckDBs auto CSV loading
+		// https://duckdb.org/docs/data/csv/overview.html
+		const quotedQueryPath = `'${queryPath}'`;
+		const optionsArray = options?.split(',') ?? [];
+		if (!options?.toLowerCase().includes('auto_detect')) {
+			optionsArray.push('auto_detect = true');
+		}
 
-  // This function will be called for EVERY file in the sources directory
-  // If you are expecting a specific file type (e.g. SQL files), make sure to filter
-  // to exclude others.
-
-  // If you are using some local database file (e.g. a sqlite or duckdb file)
-  // You may also need to filter that file out as well
-  return async (queryText, queryPath) => {
-    // Note: add your logic to process each file queryText and/or queryPath here
-    // ...
-    
-    // Example output, delete or modify as needed
-    const output = {
-      rows: [
-        { someInt: 1, someString: "string" },
-        { someInt: 2, someString: "string2" },
-      ],
-      columnTypes: [
-        {
-          name: "someInt",
-          evidenceType: EvidenceType.NUMBER,
-          typeFidelity: "inferred",
-        },
-        {
-          name: "someString",
-          evidenceType: EvidenceType.STRING,
-          typeFidelity: "inferred",
-        },
-      ],
-      expectedRowCount: 2,
-    };
-
-    return output;
-  };
+		return runQuery(
+			`SELECT * FROM read_csv(${[quotedQueryPath, ...optionsArray].join(', ')})`,
+			{ filename: ':memory:' },
+			batchSize
+		);
+	};
 };
 
-// Uncomment to use the advanced source interface
-// This uses the `yield` keyword, and returns the same type as getRunner, but with an added `name` and `content` field (content is used for caching)
-// sourceFiles provides an easy way to read the source directory to check for / iterate through files
-// /** @type {import("@evidence-dev/db-commons").ProcessSource<ConnectorOptions>} */
-// export async function* processSource(options, sourceFiles, utilFuncs) {
-//   yield {
-//     title: "some_demo_table",
-//     content: "SELECT * FROM some_demo_table", // This is ONLY used for caching
-//     rows: [], // rows can be an array
-//     columnTypes: [
-//       {
-//         name: "someInt",
-//         evidenceType: EvidenceType.NUMBER,
-//         typeFidelity: "inferred",
-//       },
-//     ],
-//   };
-//   yield {
-//     title: "some_demo_table",
-//     content: "SELECT * FROM some_demo_table", // This is ONLY used for caching
-//     rows: async function* () {}, // rows can be a generator function for returning batches of results (e.g. if an API is paginated, or database supports cursors)
-//     columnTypes: [
-//       {
-//         name: "someInt",
-//         evidenceType: EvidenceType.NUMBER,
-//         typeFidelity: "inferred",
-//       },
-//     ],
-//   };
-
-//  throw new Error("Process Source has not yet been implemented");
-// }
-
-/**
- * Implementing this function creates an "advanced" connector
- *
- *
- * @see https://docs.evidence.dev/plugins/create-source-plugin/
- * @type {import("@evidence-dev/db-commons").GetRunner<ConnectorOptions>}
- */
-
-/** @type {import("@evidence-dev/db-commons").ConnectionTester<ConnectorOptions>} */
+/** @type {import("@evidence-dev/db-commons").ConnectionTester<DuckDBOptions>} */
 export const testConnection = async (opts) => {
-  return true;
+	const r = await runQuery('SELECT 1;', { ...opts, filename: ':memory:' })
+		.then(exhaustStream)
+			.then(() => true)
+			.catch((e) => ({ reason: e.message ?? 'File not found' }));
+	return r;
+};
+
+export const options = {
+	options: {
+		title: 'Options',
+		description:
+			"String passed directly to duckdb's CSV import. See https://duckdb.org/docs/data/csv/overview.html for available configuration",
+		type: 'string',
+		secret: false,
+		shown: true,
+		virtual: false,
+		nest: false,
+		default: ''
+	}
 };
